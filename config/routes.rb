2b676314c979819acab19d63cdc0120c9816c3a8
Jason Mannon <jmannonpdx@gmail.com>
2014-12-18 14:25:36 -0800

    Sets up analytics controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   mount Api::API => '/'
+  resources :analytics, only: :index
+  root "analytics#index"
 end
