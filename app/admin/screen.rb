5ebf025d2877373d2ddd9133b49a74f7bb5fcc2c
Jason Mannon <jmannonpdx@gmail.com>
2014-12-17 17:09:29 -0800

    Sets up admin screen crud

--- app/admin/screen.rb
+++ app/admin/screen.rb
@@ -1,4 +1,23 @@
 ActiveAdmin.register Screen do
   menu priority: 2
   permit_params :list, :of, :attributes, :on, :screen, :image, :image_name, :sequence
+
+  form :html => {:multipart => true} do |f|
+    inputs 'Screen' do
+      input :sequence
+      input :image, :as => :file
+    end
+    para "Press cancel to return to the list without saving."
+    actions
+  end
+
+  show do |screen|
+    attributes_table do
+      row :id
+      row :sequence
+      row :image do
+        image_tag(screen.image.url)
+      end
+    end
+  end
 end
