6a23a8aae4d642a7b04a1c10e628122261b73f9e
Jason Mannon <jmannonpdx@gmail.com>
2014-12-18 16:20:02 -0800

    Sets up sample analytics dashboard with controller actions to grab parse data

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -1,4 +1,5 @@
 class AnalyticsController < ApplicationController
   def index
+    @visitors = Parse::Query.new("visitor").get
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
 
   def authenticate
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["USER_NAME"] && password == ENV["PASSWORD"]
+      username == "foo" && password == "bar"
     end
   end
 end
